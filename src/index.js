9ba7c1894d15bdc7a152b93efd7711794e09d6a4
sonastea <sonastea@gmail.com>
2024-01-10 14:33:02 -0800

    build(package.json): change project name

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 import { PORT } from "./utils/environment.js";
 
 app.listen(PORT ?? 3000, () =>
-  console.log(`Backend listening on port ${PORT}!`)
+  console.log(`bit listening on port ${PORT}!`)
 );
