4ccddbca75bceebd5704856adc72d0f0b549f219
sonastea <sonastea@gmail.com>
2024-01-22 02:02:40 -0800

    test: refactor radiologists to variables export [skip ci]

--- test/routes/user.test.ts
+++ test/routes/user.test.ts
@@ -1,5 +1,10 @@
 import { app, compareArrayOfObjects } from "../helper";
-import { USER_EMAIL, USER_EMAIL_PHYSICIAN, USER_PW } from "../variables";
+import {
+  MEET_OUR_RADIOLOGISTS,
+  USER_EMAIL,
+  USER_EMAIL_PHYSICIAN,
+  USER_PW,
+} from "../variables";
 
 describe("user route", () => {
   let token: string, physicianToken: string;
@@ -46,6 +51,7 @@ describe("user route", () => {
       url: "/api/user/radiologists",
       headers: { Authorization: `Bearer ${token}` },
     });
+
     expect(response.json().radiologists?.length).toBeGreaterThan(0);
     expect(Object.keys(response.json().radiologists[0])).toEqual(
       expect.arrayContaining([
@@ -64,61 +70,13 @@ describe("user route", () => {
   });
 
   test("meet our radiologists", async () => {
-    const radiologists = [
-      {
-        uid: "464xIBql69bqrg8F1BhzHDHtHsw1",
-        title: "Dr.",
-        first_name: "Emily",
-        last_name: "Johnson",
-        profile_image_url: null,
-        expertise: "Diagnostic",
-      },
-      {
-        uid: "oNgXBgPnbAZymU51OyJ0CQ9GcFD2",
-        title: "Dr.",
-        first_name: "David",
-        last_name: "Smith",
-        profile_image_url: null,
-        expertise: "Neuroradiology",
-      },
-      {
-        uid: "xc3CMA0sMZNLRp5qDVH6UU3FQO83",
-        title: "Dr.",
-        first_name: "Robert",
-        last_name: "Rivera",
-        profile_image_url: null,
-        expertise: "Abdominal",
-      },
-      {
-        uid: "T3e7wxi3khgwiZGp7fXejUJe2Qg2",
-        title: "Dr.",
-        first_name: "John",
-        last_name: "Radiologist",
-        profile_image_url: null,
-        expertise: "Neuroradiology",
-      },
-      {
-        uid: "kMBvFu9pG2T36w8zqHf99rDiq992",
-        title: "Dr.",
-        first_name: "Laura",
-        last_name: "Davis",
-        profile_image_url: null,
-        expertise: "Musculoskeletal",
-      },
-      {
-        uid: "6npcE5FQFweAYGAxDlxSARwz1qn2",
-        title: "Dr.",
-        first_name: "Monica",
-        last_name: "Kirk",
-        profile_image_url: null,
-        expertise: "Pediatric",
-      },
-    ];
-
     const response = await app.inject({
       url: "/api/user/meet-our-radiologists",
     });
-    expect(compareArrayOfObjects(response.json(), radiologists)).toBe(true);
+
+    expect(compareArrayOfObjects(response.json(), MEET_OUR_RADIOLOGISTS)).toBe(
+      true
+    );
   });
 
   test("get user patient role", async () => {
