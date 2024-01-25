ac168705fdb18e814ca5492b590931bfe89934ac
sonastea <sonastea@gmail.com>
2024-01-25 01:09:29 -0800

    feat: isStaff middleware, needs tests

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -19,20 +19,4 @@ export const isAuthorized = async (req, res, next) => {
     console.log(error.code, error.message);
     res.status(401).send({ error: "You are not authorized to make this request" });
   }
-};
-
-// We allow if the user is a physician or radiologist
-export const isStaff = async (req, res, next) => {
-  try {
-    const user = await sql`SELECT uid, role FROM "User" WHERE uid = ${req.userUID}`;
-
-    if (user.count > 0 && ["PHYSICIAN", "RADIOLOGIST"].includes(user[0].role)) {
-      next();
-    } else {
-      res.status(401).send({ error: "You are not authorized to make this request" });
-    }
-  } catch (error) {
-    console.log(error.code, error.message);
-    res.status(401).send({ error: "You are not authorized to make this request" });
-  }
 }; */
